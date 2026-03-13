69961fc9f86ea876e5a1490b5c8dd3d3e849ab84
Yahiko5677 <Yahiko5679+Ayanokoji@gmail.com>
2026-03-14 00:10:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,6 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            plugins=dict(root="handlers"),
             sleep_threshold=10,
         )
 
