455dbc8b04573827d2d12a028493d693946d40e4
Yahiko5677 <Yahiko5679+Ayanokoji@gmail.com>
2026-03-14 15:02:09 +0530

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,13 +1,12 @@
 from .db import (
     ensure_indexes,
     save_pending_file,
-    get_pending_episodes,
-    get_all_pending,
     mark_posted,
-    delete_pending_by_id,
     get_settings,
     update_settings,
     add_channel,
     remove_channel,
+    update_channel_qualities,
+    set_quality_bot,
     get_stats,
 )
