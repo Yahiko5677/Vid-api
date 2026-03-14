30e1310b9e88dd1f3209a4be878a39f05fa7aa50
Yahiko5677 <Yahiko5679+Ayanokoji@gmail.com>
2026-03-14 16:10:21 +0530

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,7 +1,10 @@
 from .builder import (
     settings_menu,
+    quality_bots_menu,
     channel_manager,
+    channel_quality_picker,
     channel_picker,
+    quality_buttons,
     confirm_upload,
     post_confirm,
     force_post_keyboard,
